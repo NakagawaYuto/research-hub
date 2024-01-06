e7e385d03a6c7efe9246f81fd5d6394a809443c6
Itou-Ayaka <ayaka.peridot828@gmail.com>
2024-01-06 14:03:00 +0000

    データベースを作成した．

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+class Task(models.Model):
+    title=models.CharField(max_length=255)
+    deadline = models.DateField()
+    created_date=models.DateField(auto_now_add=True)
+    
 
+class detail(models.Model):
+    detail_title=models.CharField(max_length=255)
+    detail_deadline = models.DateField()
+    detail_created_date=models.DateField(auto_now_add=True)
+    department = models.ForeignKey(Task, on_delete=models.CASCADE)
 
 
 
