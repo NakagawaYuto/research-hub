63a75e25572e0fa53ea93cb1a76a7307ba4d628e
Itou-Ayaka <ayaka.peridot828@gmail.com>
2024-01-06 16:21:16 +0000

    task_list.htmlの作成，ルーティングを行った．

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Task
 
 # Create your views here.
+class TodoList(ListView):
+    model = Task
+    context_object_name = "tasks"
+
