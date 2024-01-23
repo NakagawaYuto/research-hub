d7c7a58567f57d1f18b76a048bb928cd29fd4476
Toshiharu Minematsu <mine.t20020825@icloud.com>
2024-01-23 22:15:04 +0900

    データベースの作成

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User,Theme,Novelty
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Theme)
+admin.site.register(Novelty)
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    student_id = models.CharField(max_length=50)
+    research_theme = models.CharField(max_length=200)
+    # TODO: 技術タグ
+
+class Theme(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+class Novelty(models.Model):
+    description = models.TextField()
