17af23d65ec132b3d3968ace79eb089cebdf937e
Toshiharu Minematsu <mine.t20020825@icloud.com>
2024-02-25 12:02:06 +0900

    ビューの設定

--- backend/home/views.py
+++ backend/home/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Theme, Novelty
+from .serializer import UserSerializer, ThemeSerializer, NoveltySerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class ThemeViewSet(viewsets.ModelViewSet):
+    queryset = Theme.objects.all()
+    serializer_class = ThemeSerializer
+
+
+class NoveltyViewSet(viewsets.ModelViewSet):
+    queryset = Novelty.objects.all()
+    serializer_class = NoveltySerializer
