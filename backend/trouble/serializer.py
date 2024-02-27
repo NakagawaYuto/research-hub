5915958c9ad9da7be44922f1e0af56eb36660a85
Keisuke HARADA <21238330@edu.cc.saga-u.ac.jp>
2024-02-27 20:31:29 +0900

    バックエンドの修正

--- backend/trouble/serializer.py
+++ backend/trouble/serializer.py
@@ -2,13 +2,15 @@ from rest_framework import serializers
 from .models import Trouble, Comment
 
 
-class TroubleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Trouble
-        fields = '__all__'
-
-
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
+        fields = '__all__'
+
+
+class TroubleSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Trouble
         fields = '__all__'
