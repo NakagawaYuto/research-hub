b6ad3ba507d01e01480a0916eb0c3f27f5ceb081
Toshiharu Minematsu <mine.t20020825@icloud.com>
2024-02-28 16:31:59 +0900

    ルーティング

--- front/src/App.js
+++ front/src/App.js
@@ -2,12 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import HomePage from './Pages/Homepage';
+import UserPage from './Pages/UserPage';
+import AddUserPage from './Pages/AddUserPage';
+import ThemePage from './Pages/ThemePage';
+import MemoPage from './Pages/MemoPage';
+import NoveltyPage from './Pages/NoveltyPage';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/user/add" element={<AddUserPage />} />
+        <Route path="/user/:id" element={<UserPage />} />
+        <Route path="/user/:id/theme" element={<ThemePage />} />
+        <Route path="/user/:id/memo" element={<MemoPage />} />
+        <Route path="/user/:id/novelty" element={<NoveltyPage />} />
       </Routes>
     </div>
   );
