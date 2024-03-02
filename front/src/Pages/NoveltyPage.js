e2c49feb2acb514a8eb7484db763da6840782de3
Toshiharu Minematsu <mine.t20020825@icloud.com>
2024-03-02 13:05:56 +0900

    新規性を編集できるようにした

--- front/src/Pages/NoveltyPage.js
+++ front/src/Pages/NoveltyPage.js
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { Container, Typography, Grid, Button, Box, TextField } from '@mui/material';
 
+const baseURL = 'http://127.0.0.1:8080/users/';
 
 const NoveltyPage = () => {
-    const novelty = "Write your novelty here";
+    const [novelty, setNovelty] = useState(null);
+    const { id } = useParams();
+
+    useEffect(() => {
+        const fetchData = () => {
+            try {
+                axios.get(`${baseURL}${id}/`).then((response) => {
+                    setNovelty(response.data.novelty);
+                });
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, [id]);
+
+    const saveNovelty = () => {
+        try {
+            axios.patch(`${baseURL}${id}/`, { novelty: novelty });
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
-        <div>
-            <h1>Novelty Page</h1>
-            <input type="text" placeholder="Enter your novelty" />
-            <button>Save</button>
-            <p>{novelty}</p>
-        </div>
+        <Container>
+            <Box sx={{ my: 4 }}>
+                <Typography variant='h4' align='center'>
+                    Novelty Page
+                </Typography>
+            </Box>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <TextField
+                        fullWidth
+                        variant='outlined'
+                        placeholder='Enter your novelty'
+                        value={novelty}
+                        onChange={(e) => setNovelty(e.target.value)}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant='contained' color='primary' onClick={saveNovelty}>
+                        Save
+                    </Button>
+                </Grid>
+            </Grid>
+        </Container>
     );
-}
+};
 
 export default NoveltyPage;
