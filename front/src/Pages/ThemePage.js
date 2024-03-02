d24d6b0c630354fc1f7a1571c03a190a93f15fb5
Toshiharu Minematsu <mine.t20020825@icloud.com>
2024-03-02 13:05:45 +0900

    テーマを編集できるようにした

--- front/src/Pages/ThemePage.js
+++ front/src/Pages/ThemePage.js
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { Container, Typography, Grid, Button, Box, TextField } from '@mui/material';
 
+const baseURL = 'http://127.0.0.1:8080/users/';
 
 const ThemePage = () => {
-    const researchTheme = "Write your theme here";
+    const [theme, setTheme] = useState(null);
+    const { id } = useParams();
+
+    useEffect(() => {
+        const fetchData = () => {
+            try {
+                axios.get(`${baseURL}${id}/`).then((response) => {
+                    setTheme(response.data.research_theme);
+                });
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, [id]);
+
+    const saveTheme = () => {
+        try {
+            axios.patch(`${baseURL}${id}/`, { research_theme: theme });
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
-        <div>
-            <h1>Theme Page</h1>
-            <input type="text" placeholder="Enter your theme" />
-            <button>Save</button>
-            <p>{researchTheme}</p>
-        </div>
+        <Container>
+            <Box sx={{ my: 4 }}>
+                <Typography variant='h4' align='center'>
+                    Theme Page
+                </Typography>
+            </Box>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <TextField
+                        fullWidth
+                        variant='outlined'
+                        placeholder='Enter your research theme'
+                        value={theme}
+                        onChange={(e) => setTheme(e.target.value)}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant='contained' color='primary' onClick={saveTheme}>
+                        Save
+                    </Button>
+                </Grid>
+            </Grid>
+        </Container>
     );
-}
+};
 
 export default ThemePage;
