92439cdc360d87d994496275608a2b9cf995cf64
Keisuke HARADA <21238330@edu.cc.saga-u.ac.jp>
2024-03-05 18:04:56 +0900

    編集ページを作り，ルーティングを行った

--- front/src/App.js
+++ front/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import TroublePage from './Pages/TroublePage';
 import AddPage from './Pages/AddPage';
 import TroubleDetailPage from './Pages/TroubleDetailPage';
+import EditPage from './Pages/EditPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<TroublePage/>} />
         <Route path="/add/" element={<AddPage />} />
         <Route path="/trouble/:id" element={<TroubleDetailPage/>}/>
+        <Route path="/edit/:id" element={<EditPage />} />
       </Routes>
     </div>
   );
