3470371d1e69b1d9191dd2c80d131202b53f10a1
Keisuke HARADA <21238330@edu.cc.saga-u.ac.jp>
2024-03-05 18:04:38 +0900

    メニューボタンを作った

---
+++ front/src/Components/DetailButton.js
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { useNavigate } from 'react-router-dom';
+
+const options = [
+  '編集',
+  '削除',
+];
+
+const ITEM_HEIGHT = 48;
+
+export default function LongMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const navigate = useNavigate();
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleEdit = () => { // 編集ボタンがクリックされたときの処理
+    navigate('/edit'); // 編集ページに遷移
+    handleClose(); // メニューを閉じる
+  };
+
+  const handleDelete = () => { // 削除ボタンがクリックされたときの処理
+    //削除するか表示する機能を作る
+
+    handleClose(); // メニューを閉じる
+  };
+
+  return (
+    <div>
+      <IconButton
+        aria-label="more"
+        id="long-button"
+        aria-controls={open ? 'long-menu' : undefined}
+        aria-expanded={open ? 'true' : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        id="long-menu"
+        MenuListProps={{
+          'aria-labelledby': 'long-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: '20ch',
+          },
+        }}
+      >
+        <MenuItem key="edit" onClick={handleEdit}>編集</MenuItem> {/* 編集ボタン */}
+        <MenuItem key="delete" onClick={handleDelete}>削除</MenuItem> {/* 削除ボタン */}
+      </Menu>
+    </div>
+  );
+}
--- front/src/Components/TroubleCards.js
+++ front/src/Components/TroubleCards.js
@@ -5,6 +5,9 @@ import CardContent from '@mui/material/CardContent';
 import CardActionArea from '@mui/material/CardActionArea';
 import { useNavigate } from 'react-router-dom';
 
+import DetailButton from '../Components/DetailButton';
+
+
 const TroubleCards = ({ Troubles }) => {
   const navigate = useNavigate();
   var Cards = [];
@@ -20,6 +23,11 @@ const TroubleCards = ({ Troubles }) => {
           elevation={4} 
           style={{
             margin: 10,
+            //ここで詳細ボタンの位置を調整
+            display: 'flex',
+            justifyContent: 'space-between', // カードの内部で要素を左右に配置
+            alignItems: 'center', // カードの内部で要素を中央に配置
+            //ここで詳細ボタンの位置を調整
           }}
         >
           <CardActionArea onClick={() => { navigate('/trouble/'+String(trouble.id))}}>
@@ -30,8 +38,10 @@ const TroubleCards = ({ Troubles }) => {
 
             </CardContent>
           </CardActionArea>
+          <DetailButton />
         </Card>
       </Grid>
+      
     );
   }
   return (
