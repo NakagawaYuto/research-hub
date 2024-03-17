bdc265dd80afb919b87fbd3d43ee262f61feefc1
Toshiharu Minematsu <mine.t20020825@icloud.com>
2024-03-17 16:58:00 +0900

    ユーザー追加ページの完成（暫定）

--- front/src/Pages/AddUserPage.js
+++ front/src/Pages/AddUserPage.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Box, Button, Grid, IconButton, TextField, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import { Container, Typography, Button, Box, TextField, IconButton } from '@mui/material';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 const baseURL = 'http://127.0.0.1:8080/users/';
@@ -14,18 +13,14 @@ const pageStyle = {
 };
 
 const textFieldStyle = {
-  mb: 2,
+  mb: 6,
   bgcolor: 'white',
   borderRadius: '5px',
 };
 
 const buttonStyle = {
-  position: 'absolute',
-  right: 0,
-  bottom: -120,
   width: '150px',
   height: '50px',
-  mt: 2,
   bgcolor: '#1976d2',
   '&:hover': {
     bgcolor: '#115293',
@@ -59,47 +54,55 @@ const AddUserPage = () => {
     <div style={pageStyle}>
       <Header />
 
-      <Container maxWidth="lg">
-        <IconButton onClick={() => navigate('/')} style={{ fontFamily: 'Meiryo', fontSize: '20px', fontWeight: 'bold', color: '#666', marginTop: '0px', marginLeft: '20px' }}>
-          <ArrowBackIosIcon />
-          <Typography>
-            ユーザーページへ戻る
-          </Typography>
-        </IconButton>
-        <Typography variant='h4' align='center' sx={{ mt: 6 }}>
-          ユーザー追加
-        </Typography>
-        <Box sx={{ position: 'relative', mt: 8 }}>
-          <TextField
-            fullWidth
-            variant='outlined'
-            placeholder='名前'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-            sx={textFieldStyle}
-          />
-          <TextField
-            fullWidth
-            variant='outlined'
-            placeholder='学籍番号'
-            value={student_id}
-            onChange={(e) => setStudent_id(e.target.value)}
-            required
-            sx={textFieldStyle}
-          />
-          <Button
-            variant='contained'
-            onClick={addUser}
-            sx={buttonStyle}
-          >
-            <Typography variant="h6">
-              追加
+      <Grid container item xs={12}>
+        <Grid item xs={3}>
+          <IconButton onClick={() => navigate('/')} style={{ fontFamily: 'Meiryo', fontSize: '20px', fontWeight: 'bold', color: '#666', marginTop: '0px', marginLeft: '20px' }}>
+            <ArrowBackIosIcon />
+            <Typography>
+              戻る
             </Typography>
-          </Button>
-        </Box>
-      </Container>
-    </div>
+          </IconButton>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant='h4' style={{ fontFamily: 'Meiryo' }} align='center' sx={{ mt: 4, mb: 6 }}>
+            ユーザー追加
+          </Typography>
+        </Grid>
+        <Grid container item xs={12} justifyContent="center">
+          <Grid item xs={6}>
+            <TextField
+              fullWidth
+              variant='outlined'
+              placeholder='名前'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+              sx={textFieldStyle}
+            />
+            <TextField
+              fullWidth
+              variant='outlined'
+              placeholder='学籍番号'
+              value={student_id}
+              onChange={(e) => setStudent_id(e.target.value)}
+              required
+              sx={textFieldStyle}
+            />
+            <Box display="flex" justifyContent="flex-end">
+              <Button
+                variant='contained'
+                onClick={addUser}
+                sx={buttonStyle}
+              >
+                <Typography variant="h6">
+                  追加
+                </Typography>
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </Grid >
+    </div >
   );
 };
 
