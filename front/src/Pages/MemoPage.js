5357b686bda0d8361546e92561016524e53cf42f
Toshiharu Minematsu <mine.t20020825@icloud.com>
2024-03-18 23:01:59 +0900

    メモページの完成（暫定）

--- front/src/Pages/MemoPage.js
+++ front/src/Pages/MemoPage.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link, useParams } from 'react-router-dom';
-import { Button, Container, Typography, Box, TextField } from '@mui/material';
+import { Box, Button, Container, Grid, IconButton, TextField, Typography } from '@mui/material';
+import { useParams, useNavigate } from 'react-router-dom';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import Header from '../components/Header';
 
 const baseURL = 'http://127.0.0.1:8080/users/';
+
+const pageStyle = {
+  backgroundColor: '#f5f5f5',
+  minHeight: '100vh',
+};
+
+const textFieldStyle = {
+  mb: 6,
+  bgcolor: 'white',
+  borderRadius: '5px',
+};
 
 const MemoPage = () => {
   const [memo, setMemo] = useState('');
   const { user_id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -37,41 +51,39 @@ const MemoPage = () => {
   };
 
   return (
-    <Container maxWidth="lg">
-      <Box sx={{ position: 'relative', mt: 6 }}>
-        <Link to={`/user/${user_id}`} style={{ textDecoration: 'none', position: 'absolute', top: '-25px', left: 0 }}>
-          <Button
-            variant='contained'
-            sx={{
-              width: '65px',
-              height: '40px',
-              backgroundColor: 'white',
-              color: 'black',
-              borderColor: 'black',
-              borderWidth: 2,
-              borderStyle: 'solid'
-            }}
-          >
-            <Typography variant="h7">
+    <div style={pageStyle}>
+      <Header />
+
+      <Grid container item xs={12}>
+        <Grid item xs={3}>
+          <IconButton onClick={() => navigate(`/user/${user_id}`)} style={{ fontFamily: 'Meiryo', fontSize: '20px', fontWeight: 'bold', color: '#666', marginTop: '0px', marginLeft: '20px' }}>
+            <ArrowBackIosIcon />
+            <Typography>
               戻る
             </Typography>
-          </Button>
-        </Link>
-        <Typography variant='h4' align='center'>
-          メモ
-        </Typography>
-        <TextField
-          fullWidth
-          multiline
-          rows={8}
-          variant='outlined'
-          placeholder='メモを入力してください。'
-          value={memo}
-          onChange={handleMemoChange}
-          sx={{ mt: 2 }}
-        />
-      </Box>
-    </Container>
+          </IconButton>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant='h4' style={{ fontFamily: 'Meiryo' }} align='center' sx={{ mt: 4, mb: 6 }}>
+            メモ
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container item xs={12} justifyContent="center">
+        <Grid item xs={6}>
+          <TextField
+            fullWidth
+            multiline
+            rows={8}
+            variant='outlined'
+            placeholder='メモを入力してください。'
+            value={memo}
+            onChange={handleMemoChange}
+            sx={textFieldStyle}
+          />
+        </Grid>
+      </Grid>
+    </div>
   );
 };
 
