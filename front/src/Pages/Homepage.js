19205cd2b037ce88bd184f89a4e0dcdb4d33dc71
Toshiharu Minematsu <mine.t20020825@icloud.com>
2024-03-18 23:56:26 +0900

    タイムラインの作成

--- front/src/Pages/Homepage.js
+++ front/src/Pages/Homepage.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Box, Grid, Typography } from '@mui/material';
+import { Box, Grid, Typography, Card, CardContent, Divider } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 import UserCard from '../components/UserCard';
 import AddButton from '../components/AddButton';
 
 const baseURL = 'http://127.0.0.1:8080/users/';
+const troubleURL = 'http://127.0.0.1:8080/trouble/trouble/';
 
 const pageStyle = {
   backgroundColor: '#f5f5f5',
@@ -15,9 +16,10 @@ const pageStyle = {
 
 const HomePage = () => {
   const [users, setUsers] = useState([]);
+  const [troubles, setTroubles] = useState([]);
   const navigate = useNavigate();
 
-  const fetchData = async () => {
+  const fetchUsers = async () => {
     try {
       const response = await axios.get(baseURL);
       setUsers(response.data);
@@ -26,8 +28,19 @@ const HomePage = () => {
     }
   };
 
+  const fetchTroubles = async () => {
+    try {
+      const response = await axios.get(troubleURL);
+      const sortedData = response.data.sort((a, b) => new Date(b.created_date) - new Date(a.created_date));
+      setTroubles(sortedData);
+    } catch (error) {
+      console.error('There was an error fetching the troubles:', error);
+    }
+  };
+
   useEffect(() => {
-    fetchData();
+    fetchUsers();
+    fetchTroubles();
   }, []);
 
   return (
@@ -45,7 +58,22 @@ const HomePage = () => {
             <Typography variant='h4' gutterBottom>
               タイムライン
             </Typography>
-            {/* タイムラインのコンテンツをここに追加 */}
+            {troubles.map((trouble) => (
+              <Card key={trouble.id} sx={{ marginBottom: '20px' }}>
+                <CardContent>
+                  <Typography variant='h5' component='div'>
+                    {trouble.title}
+                  </Typography>
+                  <Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
+                    作成日: {trouble.created_date}
+                  </Typography>
+                  <Typography variant='body2'>
+                    {trouble.body}
+                  </Typography>
+                </CardContent>
+                <Divider />
+              </Card>
+            ))}
           </Grid>
         </Grid>
       </Box>
